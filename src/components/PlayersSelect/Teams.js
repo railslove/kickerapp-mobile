76d5b89ba5e411d78d5fe11ba0b416cd4528aff0
Amed Kino <amed@xbyte.team>
2018-07-30 15:14:49 +0200

    Teams placeholder enhance

--- src/components/PlayersSelect/Teams.js
+++ src/components/PlayersSelect/Teams.js
@@ -7,15 +7,47 @@ type Props = {
   playersArray: PropTypes.array
 }
 
+const Team = ({children, title} : {children: any, title: string}) => (
+  <View style={stylesTeam.container}>
+    <View style={stylesTeam.childrenContainer}>
+      <Text>{children}</Text>
+    </View>
+    <View style={stylesTeam.titleContainer}>
+      <Text style={stylesTeam.title}>{title}</Text>
+    </View>
+  </View>
+)
+
+const stylesTeam = {
+  container:{
+    flexDirection: 'column'
+  },
+  childrenContainer: {
+    flexDirection: 'row'
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    paddingVertical: 5
+  },
+  title: {
+    fontSize: 17,
+    textAlign: 'center',
+    flex: 1
+  }
+}
 const Teams = ({playersArray}: Props) => (
   <View style={stylesTeams.wrapper}>
 
     <View style={stylesTeams.container}>
-      <SinglePlayerContainer />
-      <SinglePlayerContainer />
+      <Team title='TEAM A'>
+        <SinglePlayerContainer playerObject={playersArray[0]} />
+        <SinglePlayerContainer playerObject={playersArray[1]} />
+      </Team>
       <PlatformIcon androidIcon='minimize' iosIcon='md-remove' size={30} style={stylesTeams.icon} />
-      <SinglePlayerContainer />
-      <SinglePlayerContainer />
+      <Team title='TEAM B'>
+        <SinglePlayerContainer playerObject={playersArray[2]} />
+        <SinglePlayerContainer playerObject={playersArray[3]} />
+      </Team>
     </View>
 
   </View>
@@ -42,22 +74,34 @@ const stylesTeams = {
 }
 
 const playerPlaceholder = require('../../assets/images/player.png')
-const SinglePlayerContainer = ({name}) => (
-  <View style={stylesSinglePlayer.wrapper}>
-    <ImageBackground source={playerPlaceholder} style={{width: '100%', height: '100%'}}>
-      <Text>A Name</Text>
-    </ImageBackground>
-  </View>
-)
+type SinglePlayerProps = {
+  playerObject: PropTypes.object
+}
 
+const SinglePlayerContainer = ({playerObject}: SinglePlayerProps) => {
+  console.log({playerObject})
+  return (
+    <View style={stylesSinglePlayer.wrapper}>
+      <ImageBackground source={playerPlaceholder} style={stylesSinglePlayer.imageBackground}>
+        <View style={stylesSinglePlayer.container}>
+          <Text>A Name</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
 const stylesSinglePlayer = {
-  wrapper: {
+  wrapper: { padding: 2 },
+  container: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colors.avatarPlaceholder,
-    margin: 1,
     width: (screenWidth - 80) / 4,
     height: 80
+  },
+  imageBackground: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.avatarPlaceholder
   }
 }
 
